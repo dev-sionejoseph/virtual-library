70c09218dbc8978cd9121a368d91c60e0a77f190
Sione <dev.sionejoseph@gmail.com>
2024-09-10 14:11:16 -0500

    Books class completed, 8 tests passing

--- classes/Book.js
+++ classes/Book.js
@@ -1,6 +1,24 @@
 // import the Media class:
-
+const Media = require("./Media");
 // create your Book class:
+class Book extends Media{
+    constructor(title, year, genre, author, numPages, rating){
+        super(title, year, genre);
+        this.author = author;
+        this.numPages = numPages;
+        this.rating = rating;
+    }
 
+    summary(){
+        return `Title: ${this.title}, Author: ${this.author}, Year: ${this.year}, Page Count: ${this.numPages}, Genre: ${this.genre}, Rating: ${this.rating}`
+    }
+
+    static highestRating(books){
+
+        books.sort((b1,b2) => b2.rating - b1.rating)
+
+        return books[0];
+    }
+}
 // don't change below
 module.exports = Book;
--- classes/Media.js
+++ classes/Media.js
@@ -1,10 +1,13 @@
 // create your Media class:
 class Media{
     static totalMediaCount = 0;
-    constructor(title, genre, year){
+    
+    constructor(title, year, genre){
         this.title = title;
         this.genre = genre;
         this.year = year;
+
+        Media.totalMediaCount++;
     }
 
     summary(){
