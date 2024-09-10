2aff161cdcffb008c83190272d0d8c88b1fdad2b
Sione <dev.sionejoseph@gmail.com>
2024-09-10 14:35:23 -0500

    Music class completed, all tests passing

--- classes/Music.js
+++ classes/Music.js
@@ -1,6 +1,25 @@
 // import the Media class:
+const Media = require("./Media");
 
 // create your Music class:
+
+class Music extends Media {
+    constructor(title, year, genre, artist, length){
+        super(title, year, genre);
+        this.artist = artist;
+        this.length = length;
+    }
+
+    summary(){
+        return `Title: ${this.title}, Artist: ${this.artist}, Year: ${this.year}, Genre: ${this.genre}, Length: ${this.length} seconds`
+    }
+
+    static shortestAlbum(albums){
+        albums.sort((a1,a2) => a1.length - a2.length);
+
+        return albums[0];
+    }
+}
 
 // don't change below
 module.exports = Music;
